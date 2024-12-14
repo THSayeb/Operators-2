444de4e8b465d809afa83ba360a7866f09bd039f
THSayeb <desktop499@gmail.com>
2024-12-14 20:21:29 +0600

    Operators

---
+++ AND_OR_Operator.py
@@ -0,0 +1,22 @@
+a = 20 
+b = 34
+c = 323
+if a and b and c:
+    print("All of the numbers have boolean value as True")
+else:
+    print("All of the numbers have boolean value as False")
+
+
+a = 10 
+b = 3
+if a < 5 or b < 5:
+    print("Either of the numbers is biger smaller than 5")
+else:
+    print("None of the numbers is smaller than 5")
+
+a = 45
+b = 23
+if a < 0 or b < 0:
+    print("One of the number is negative")
+else:
+    print("None of the number is negative")
---
+++ Not_equal_operator.py
@@ -0,0 +1,26 @@
+a = 4
+b = 5
+c = 5
+print(a != b)
+print(b != c)
+
+
+a = "python"
+b = "coding"
+if a != b:
+    print(a, " and ", b, " is different")
+else:
+    print(a, " and ", b, " is same")
+
+
+a = 4
+b = 5
+if (a == 34) != (b == 5):
+    print("Hello")
+
+
+num = int(input("Enter a number "))
+if num%2 != 0:
+    print(num, " is not a even number")
+else:
+    print("It is even number")
